151e25a9b2f9378abd8d93dcec8c6f6a48455f5b
vaiz777 <moch.nadhiful94@gmail.com>
2022-09-18 15:48:53 +0700

    first commit

---
+++ src/api/travelpackage/controllers/travelpackage.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * travelpackage controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::travelpackage.travelpackage');
---
+++ src/api/travelpackage/routes/travelpackage.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * travelpackage router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::travelpackage.travelpackage');
